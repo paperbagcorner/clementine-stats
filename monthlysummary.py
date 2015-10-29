f61b930d2db867e3d51bf867e638290072f7764d
Mattias Andersson <mandersson444@gmail.com>
2015-10-29 22:23:53 +0100

    Enclose the database operations in a with statement

--- monthlysummary.py
+++ monthlysummary.py
@@ -16,11 +16,11 @@ DB_FILE = os.path.join(os.environ['HOME'], '.config/Clementine/clementine.db')
 
 
 def GetDataFromDb(filename):
-    ''' 
+    '''
     Opens the database file filename and performs the query.
 
     Args:
-      filename: a string with a valid filename to an existing 
+      filename: a string with a valid filename to an existing
                 Clementine database.'
     Returns:
       A list of sqlrows where the elements can be referenced by name.
@@ -28,30 +28,33 @@ def GetDataFromDb(filename):
     # Create the database connection
     conn = sqlite3.connect(DB_FILE)
     conn.row_factory = sqlite3.Row
-    
-    # Get the data.
-    cur = conn.cursor()
-    cur.execute("SELECT strftime('%Y-%m', lastplayed, 'unixepoch') as month, "
-                "Count(*) as num_songs, "
-                "Sum(length) / 1000000000 as length_secs "  # Nanoseconds in db.
-                "FROM songs WHERE unavailable = 0 "
-                "GROUP BY strftime('%Y-%m', lastplayed, 'unixepoch') "
-                "ORDER BY lastplayed")
 
-    stats = cur.fetchall()
-    conn.close()
+    # Get the data.
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT "
+                    "strftime('%Y-%m', lastplayed, 'unixepoch') as month, "
+                    "Count(*) as num_songs, "
+                    "Sum(length) / 1000000000 as length_secs "  # Nanoseconds
+                                                                # in db.
+                    "FROM songs WHERE unavailable = 0 "
+                    "GROUP BY strftime('%Y-%m', lastplayed, 'unixepoch') "
+                    "ORDER BY lastplayed")
+
+        stats = cur.fetchall()
+    # conn.close()
 
     return stats
 
 def BuildResultList(stats):
     '''
-    Constructs a list of dictionaries from the sqlrows returned by 
-    GetDataFromDb and adds entries for those months for which there was no 
+    Constructs a list of dictionaries from the sqlrows returned by
+    GetDataFromDb and adds entries for those months for which there was no
     last played songs.
 
-    Args: 
+    Args:
       stats: A list of sqlrows.
-    Returns: 
+    Returns:
       A list of dictionaries where all months from the first
       month in the result from the db query until today is present event if
       no songs where last played during certain months.
@@ -76,21 +79,21 @@ def BuildResultList(stats):
             row['num_songs'] = 0
             row['length_sec'] = 0
         month_list.append(row)
-        
+
         month = month + relativedelta(months=+1)
 
     return month_list
-        
+
     # for stat in stats:
     #     months[stat['month']] = stat['num_songs']
     # return months
 
 def PrintResultList(a_list):
-    ''' 
-    Prints a nicely formatted list of the result. 
+    '''
+    Prints a nicely formatted list of the result.
 
     Args:
-      a_list: A list of dictionaries which has the keys month, num_songs 
+      a_list: A list of dictionaries which has the keys month, num_songs
               and length_sec.
     Returns:
       None
@@ -108,5 +111,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
