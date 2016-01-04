a9bdd61d521151d375e988b4a61340c22df44443
Mattias Andersson <mandersson444@gmail.com>
2016-01-04 18:50:44 +0100

    Tweak install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Install clstatistics.py to the $HOME/bin directory.
-install clstatistics.py $HOME/bin
+install clstatistics.py "${HOME}/bin/clstatistics"
+install monthlysummary.py "${HOME}/bin/monthlysummary"
