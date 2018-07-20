da74640d33d2cf5f5487e40428a69119cb2c5591
Mattias Andersson <mandersson444@gmail.com>
2018-07-20 15:01:59 +0200

    Store the records in a namedtuple

--- monthlysummary.py
+++ monthlysummary.py
@@ -7,11 +7,15 @@
 import os
 import sqlite3
 
+from collections import namedtuple
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 # Default location of the database file.
 DB_FILE = os.path.join(os.environ['HOME'], '.config/Clementine/clementine.db')
+
+# This namedtuple stores the record for a given month.
+MonthRecord = namedtuple('MonthRecord', 'month, num_songs, length_secs')
 
 
 def GetDataFromDb(filename):
@@ -22,11 +26,10 @@ def GetDataFromDb(filename):
       filename: a string with a valid filename to an existing
                 Clementine database.'
     Returns:
-      A list of sqlrows where the elements can be referenced by name.
+      A list of MonthRecords.
     '''
     # Create the database connection
     conn = sqlite3.connect(filename)
-    conn.row_factory = sqlite3.Row
 
     # Get the data.
     with conn:
@@ -43,40 +46,35 @@ def GetDataFromDb(filename):
                     "ORDER BY lastplayed")
 
         stats = cur.fetchall()
-
-    return stats
+    return [MonthRecord._make(stat) for stat in stats ]
 
 def BuildResultList(stats):
     '''
-    Constructs a list of dictionaries from the sqlrows returned by
+    Constructs a list of dictionaries from the MonthRecords returned by
     GetDataFromDb and adds entries for those months for which there was no
     played songs.
 
     Args:
-      stats: A list of sqlrows.
+      stats: A list of MonthRecords.
     Returns:
-      A list of dictionaries where all months from the first
+      A list of MonthRecords where all months from the first
       month in the result from the db query until today is present event if
       no songs where last played during certain months.
     '''
 
     # Since we don't get any results from the database for the months
-    # where no songs were last played, we need to create these manually.
-    month = datetime.strptime(stats[0]['month'], '%Y-%m')
+    # where no songs were last played, we need to insert these manually.
+    month = datetime.strptime(stats[0].month, '%Y-%m')
     today = datetime.today()
     month_list = []
     i = 0
     while month <= today:
-        row = {}
         month_str = datetime.strftime(month, '%Y-%m')
-        if i < len(stats) and stats[i]['month'] == month_str:
-            row = dict(stats[i]) # Since stats[i] is a sqlrow.
+        if i < len(stats) and stats[i].month == month_str:
+            month_list.append(stats[i])
             i += 1
         else:
-            row['month'] = month_str
-            row['num_songs'] = 0
-            row['length_secs'] = 0
-        month_list.append(row)
+            month_list.append(MonthRecord(month_str, 0, 0))
 
         month = month + relativedelta(months=+1)
 
@@ -87,16 +85,15 @@ def PrintResultList(a_list):
     Prints a nicely formatted list of the result.
 
     Args:
-      a_list: A list of dictionaries which has the keys month, num_songs
-              and length_secs.
+      a_list: A list of MonthRecords.
     Returns:
       None
     '''
     for row in a_list:
         print('{:>4} songs last played in {}. Total length: {}'.format(
-            row['num_songs'],
-            row['month'],
-            timedelta(seconds=row['length_secs'])))
+            row.num_songs,
+            row.month,
+            timedelta(seconds=row.length_secs)))
 
 def main():
     stats = GetDataFromDb(DB_FILE)
