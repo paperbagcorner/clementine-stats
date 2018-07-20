e0fea11c8071a897dfd1a636902df9e50cc59ef0
Mattias Andersson <mandersson444@gmail.com>
2018-07-20 14:58:22 +0200

    Adapt unittest to the use of namedtuples

--- test_monthlysummary.py
+++ test_monthlysummary.py
@@ -11,7 +11,7 @@ class TestGetDataFromDb(unittest.TestCase):
         self.assertTrue(len(self.result) > 0)
 
     def test_has_keys(self):
-        keys = self.result[0].keys()
+        keys = self.result[0]._asdict().keys()
         self.assertTrue(len(keys) > 0)
         self.assertIn('month', keys)
         self.assertIn('num_songs', keys)
@@ -27,7 +27,7 @@ class TestBuildResultList(unittest.TestCase):
         # Check that we don't include non-played songs in the result
         # list. These have a lastplayed value of -1 in the database,
         # which is reported to us the month 1969-12.
-        unplayed_songs = [x for x in self.result if x['month'] == '1969-12']
+        unplayed_songs = [x for x in self.result if x.month == '1969-12']
         self.assertEqual(len(unplayed_songs), 0)
 
     def test_result_should_be_at_least_as_long_as_stats(self):
@@ -35,4 +35,4 @@ class TestBuildResultList(unittest.TestCase):
 
     def test_last_result_row_is_current_month(self):
         current_month = datetime.strftime(datetime.today(), '%Y-%m')
-        self.assertEqual(self.result[-1]['month'], current_month)
+        self.assertEqual(self.result[-1].month, current_month)
